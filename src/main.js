857c78904db0b3a1554694329be380f6c48a8e51
eddievandeer <1255173159@qq.com>
2020-11-28 16:54:51 +0800

    feat: use vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
