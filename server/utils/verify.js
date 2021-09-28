17a6376f7630a40ede7f23790819da68d96af7d6
eddievandeer <1255173159@qq.com>
2021-09-28 22:58:41 +0800

    feat: 添加token用户鉴权

---
+++ server/utils/verify.js
@@ -0,0 +1,16 @@
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = {
+    verifyToken
+}
