68b7fa3e923957f684a55cdd0137dad5a7687c1d
eddievandeer <1255173159@qq.com>
2021-09-29 20:14:18 +0800

    feat: 添加font-awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'font-awesome/css/font-awesome.min.css'
 
 createApp(App)
     .use(store)
