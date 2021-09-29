f071d19bef2416328d55e554dffa49847e357cb8
eddievandeer <1255173159@qq.com>
2021-09-29 20:18:21 +0800

    feat: 配置前端路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,52 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
-        redirect: '/home'
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+        meta: {
+            requiresAuth: true
+        },
+        children: [
+            {
+                path: 'chat/:uid',
+                name: 'Chat',
+                props: true,
+                component: () => import('../components/Chat.vue')
+            },
+            {
+                path: 'profile/:uid',
+                name: 'Profile',
+                props: true,
+                component: () => import('../components/Profile.vue')
+            }
+        ]
     },
     {
-        path: '/home',
-        name: 'Home',
-        component: () => import('../views/Home.vue')
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () => import('../views/Register.vue')
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const loggedIn = localStorage.getItem('user')
+
+    if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
+        next('/login')
+    }
+    else {
+        next()
+    }
 })
 
 export default router
