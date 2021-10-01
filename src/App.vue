0f32e9ba5c96decea7586c64975730c85ecdca55
eddievandeer <1255173159@qq.com>
2021-10-01 21:24:43 +0800

    feat: 重写前端入口文件App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <router-link to='/'> Home</router-link>
+    <AppNav></AppNav>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <router-view />
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+    import AppNav from './components/AppNav'
+    import {
+        useStore
+    } from 'vuex'
 
     export default {
         name: 'App',
         components: {
-            HelloWorld
+            AppNav
+        },
+        setup() {
+            const store = useStore()
+
+            initStore('user', 'SET_USER_DATA')
+            initStore('conversations', 'SET_CONVERSATION_DATA')
+            initStore('lastMessage', 'SET_LAST_MESSAGES_DATA')
+            initStore('unread', 'SET_UNREAD_DATA')
+
+            function initStore(property, mutation) {
+                let item = localStorage.getItem(property)
+
+                if (item && item !== 'undefined') {
+                    const data = JSON.parse(item)
+
+                    store.commit(mutation, data)
+                }
+            }
         }
     }
 </script>
 
 <style>
+    @import url('./style/global.scss');
+
     #app {
+        min-height: 100vh;
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+        display: flex;
     }
 </style>
