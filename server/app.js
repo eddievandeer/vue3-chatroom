55b09c7d60a78493d2b3b5009600acc07c62489a
eddievandeer <1255173159@qq.com>
2021-10-16 09:57:56 +0800

    feat: 美化服务端启动时的打印信息

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const startSocket = require('./socket');
+const chalk = require('chalk');
 const userRouter = require('./routes/user.route');
 const chatRouter = require('./routes/chat.route');
+
+const pre = +new Date()
+console.log(chalk.bgBlue.black(' INFO '), 'Starting server...')
 
 app.use(express.json());
 app.use('/user', userRouter);
@@ -12,5 +16,7 @@ app.use('/chat', chatRouter);
 startSocket(server);
 
 server.listen(3000, () => {
-    console.log('server started');
+    const now = +new Date()
+    console.log('')
+    console.log(chalk.bgGreen.black(' DONE '), chalk.green(`Server started successfully in ${now - pre}ms`));
 });
